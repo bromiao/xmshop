ec623b5e43a856b446d15d61eb43e4955695c0de
Philippppe <philip@inc-acb.com>
2023-04-03 22:24:36 +0800

    xiaomi flutter project

---
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -0,0 +1,42 @@
+import 'package:get/get.dart';
+import '../../../services/searchServices.dart';
+import '../../../services/storage.dart';
+
+class SearchController extends GetxController {
+
+  String keywords="";
+  RxList historyList=[].obs;
+  @override
+  void onInit() {
+    super.onInit();
+    getHistoryData();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+  getHistoryData() async{
+    var tempList=await SearchServices.getHistoryData();
+    if (tempList.isNotEmpty){
+        historyList.addAll(tempList);
+        update();
+    }
+  }
+  clearHistoryData() async{
+    await SearchServices.clearHistoryData();
+    historyList.clear();
+    update();
+  }
+  removeHistoryData(keywords) async{
+    var tempList=await SearchServices.getHistoryData();
+    if(tempList.isNotEmpty){
+       tempList.remove(keywords);
+       await Storage.setData("searchList", tempList);
+       //注意
+       historyList.remove(keywords);
+       update();
+    }
+  }
+
+}
